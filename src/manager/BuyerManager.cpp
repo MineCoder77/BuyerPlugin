0eb951ab496deb16500b58e10a5434bab1dd3edf
MineCoder77 <107895072+MineCoder77@users.noreply.github.com>
2023-06-16 17:24:05 +0300

    Update BuyerManager.cpp

--- src/manager/BuyerManager.cpp
+++ src/manager/BuyerManager.cpp
@@ -2,11 +2,11 @@
 
 namespace Buyer {
 
-	inline const bool give_money(Player* player, money_t money_count) {
+	inline bool give_money(Player* player, money_t money_count) {
 		return LLMoneyAdd(player->getXuid(), money_count);
 	}
 
-	inline const bool buy_item(Player* player, const Item* item, uint8_t count) {
+	inline bool buy_item(Player* player, const Item* item, uint8_t count) {
 		return give_money(player, static_cast<money_t>(JsonConfig::get_instance().get_price(item->getRawNameId()) * count));
 	}
 }
